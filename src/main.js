384ec63ebb87f4a0b760af046a39c6d2bd551e4b
TARGARYEN <leocj@live.com.co>
2022-06-03 20:49:21 -0500

    rutinario

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Store from './store'
 import './assets/base.css'
-import '@/vue-loading-overlay/dist/vue-loading.css'
+import './vue-loading-overlay/src/css/index.css'
 
 const app = createApp(App)
 
